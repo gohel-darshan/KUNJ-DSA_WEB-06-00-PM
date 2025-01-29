cd30110ead8d4caf8229f9617e4c1f4d09ea090a
gohel-darshan <darshanrgohel@gmail.com>
2025-01-29 11:53:08 +0530

    first commit

---
+++ proceduaral.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+class Student {
+private:
+    int* age;  
+
+public:
+    
+    Student(int a) {
+        age = new int(a);
+    }
+
+    
+    void display() {
+        cout << "Age: " << *age << endl;
+    }
+
+    
+    ~Student() {
+        delete age;
+        cout << "Memory freed!" << endl;
+    }
+};
+
+int main() {
+    Student* s1 = new Student(20);
+    s1->display();
+
+    delete s1;  
+
+}
